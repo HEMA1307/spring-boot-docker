782aecd776730b47bbd689b651b4b959863db3e0
Balla Hemalatha <hemalathaballa5@gmail.com>
2024-11-22 15:34:20 +0530

    Tested rest endpoint

--- src/main/java/com/example/spring/boot/docker/Application.java
+++ src/main/java/com/example/spring/boot/docker/Application.java
@@ -3,11 +3,13 @@ package com.example.spring.boot.docker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 	
-	@GetMapping("/displayMessage")
+	@GetMapping("/message")
 	public String displayMessage() {
 		//System.out.println("Successfully dockerzised");
 		return "Done..Successfully dockerzised..";
